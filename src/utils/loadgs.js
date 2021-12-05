5a2fba917814cd0049d9e2f5e62a09cfc5c59c0e
Emily Fan <emilyfan8@gmail.com>
2021-12-05 15:36:15 -0500

    copied from QuodAI calendar loading

---
+++ src/utils/loadgs.js
@@ -0,0 +1,21 @@
+export const loadGoogleScript = () => {
+  (
+    // I feel like adding the "function" is a typo but apparently it works?
+    function(){
+      console.log("Hello")
+      const id = 'google-js'
+      const src = "https://apis.google.com/js/api.js" // Quad used platform.js
+
+      const firstJs = document.getElementsByTagName('script')[0] // because react
+
+      if(document.getElementById(id)) return;
+      else{
+        const js = document.createElement('script');
+        js.id = id
+        js.src = src;
+        js.onload = window.onGoogleScriptLoad; // fascinating
+        firstJs.parentNode.insertBefore(js, firstJs);
+
+      }
+    }());
+}
