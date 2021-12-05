741262634493cbdf73087ec8693ae05119e6b930
Emily Fan <emilyfan8@gmail.com>
2021-12-04 20:40:44 -0500

    Add priority selection in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import firebase from '../firebase';
-import { RadioGroup, TextField } from '@material-ui/core';
+
+import { RadioGroup, TextField, FormControlLabel, FormLabel, Radio } from '@material-ui/core';
 import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 
@@ -12,6 +13,7 @@ const Form = () => {
         dueDate: "",
         hardDeadline: "",
         estTime: "",
+        priority: "high"
     });
     
     const createTodo = () => {
@@ -63,14 +65,64 @@ const Form = () => {
                         <input type="radio" value="option2" />
                         Soft Deadline
                     </label>
-                </div>
+                </div> */}
                 
                 <TextField
                     variant='standard'
-                    label='How Long Will it Take'
-                    type='text'
+                    label='Estimated Hours'
+                    helperText="How Long Will the Task Take You?"
+                    type='number'
                     value={formData.estTime}
                     onChange={(e) => setFormData({...formData, estTime: e.target.value})}
+                    className='textfield'
+                    size='medium'
+                />
+                
+                
+                <br/>
+                <br/>
+
+                <RadioGroup>
+                    <FormLabel component="legend">Priority</FormLabel>  
+                    <FormControlLabel 
+                        checked={formData.priority === 'tbd'}
+                        control={<Radio />} 
+                        onChange={(e) => setFormData({...formData, priority: 'tbd'})}
+                        label="Priority To Be Determined" />
+                    <FormControlLabel 
+                        checked={formData.priority === 'vlow'}
+                        control={<Radio />} 
+                        onChange={(e) => setFormData({...formData, priority: 'vlow'})}
+                        label="Very Low" />
+                    <FormControlLabel 
+                        checked={formData.priority === 'low'}
+                        control={<Radio />} 
+                        onChange={(e) => setFormData({...formData, priority: 'low'})}
+                        label="Low" />
+                    <FormControlLabel 
+                        checked={formData.priority === 'medium'}
+                        control={<Radio />} 
+                        onChange={(e) => setFormData({...formData, priority: 'medium'})}
+                        label="Medium" />
+                    <FormControlLabel 
+                        checked={formData.priority === 'high'}
+                        control={<Radio />} 
+                        onChange={(e) => setFormData({...formData, priority: 'high'})}
+                        label="High" />
+                    <FormControlLabel 
+                        checked={formData.priority === 'vHIGH'}
+                        control={<Radio />} 
+                        onChange={(e) => setFormData({...formData, priority: 'vHIGH'})}
+                        label="Very high" />
+                </RadioGroup>
+
+                <TextField
+                    disabled
+                    variant='standard'
+                    label='priority'
+                    type='text'
+                    value={formData.priority}
+                    // onChange={(e) => setFormData({...formData, estTime: e.target.value})}
                     className='textfield'
                     size='medium'
                 />
