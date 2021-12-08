a1a823896f74149165ffb3d8077fa656029ee9ce
Emily Fan <emilyfan8@gmail.com>
2021-12-07 21:29:25 -0500

    run load gapi in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import TodoApp from './components/TodoApp'
+import CalendarIntegration from './components/CalendarIntegration';
+import { loadGoogleScript } from './utils/loadgs';
+
 function App() {
+  loadGoogleScript();
+
   return (
     <>
       <div className='app'>
         <TodoApp />
       </div>
+      {/* <script async defer src="https://apis.google.com/js/api.js"
+      onload="this.onload=function(){};handleClientLoad()"
+      onreadystatechange="if (this.readyState === 'complete') this.onload()">
+    </script> */}
     </>
   );
 }
