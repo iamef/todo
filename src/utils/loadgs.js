b1726539c8fe9da7a58e4ed566cfe0d4736586c8
Emily Fan <emilyfan8@gmail.com>
2021-12-07 21:27:36 -0500

    Copy and implement gcal api quickstart

--- src/utils/loadgs.js
+++ src/utils/loadgs.js
@@ -15,3 +15,125 @@ export const loadGoogleScript = () => {
     firstJs.parentNode.insertBefore(js, firstJs);
   }
 }
+
+// copied from https://developers.google.com/calendar/api/quickstart/js
+
+
+// TIS THE LOCALHOST:8000 CLiEnT ID
+// const CLIENT_ID = '45873534951-ebrqt8r78ii9vsh5smmf0d0nllm2aa6g.apps.googleusercontent.com';
+
+const CLIENT_ID = '45873534951-sdg717toe4qqi52qh1vm9sjrt040gvjc.apps.googleusercontent.com'
+const API_KEY = 'AIzaSyCEGfVixki6n9JjL4mU9jhT6AHEtEofHP4';
+
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
+export function handleClientLoad(){
+  console.log("client load yay")
+  window.gapi.load("client:auth2", () => {
+    window.gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    }).then(function () {
+      // Listen for sign-in state changes.
+      window.gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+      // Handle the initial sign-in state.
+      updateSigninStatus(window.window.gapi.auth2.getAuthInstance().isSignedIn.get());
+      // authorizeButton.onclick = handleAuthClick;
+      // signoutButton.onclick = handleSignoutClick;
+      console.log("handle client load seemed to have worked")
+    }, function(error) {
+      console.log(JSON.stringify(error, null, 2));
+    });
+  })
+}
+
+/**
+       *  Called when the signed in status changes, to update the UI
+       *  appropriately. After a sign-in, the API is called.
+       */
+function updateSigninStatus(isSignedIn) {
+  console.log("Update sign-in status")
+  if (isSignedIn) {
+    // authorizeButton.style.display = 'none';
+    // signoutButton.style.display = 'block';
+    listUpcomingEvents();
+  } else {
+    // authorizeButton.style.display = 'block';
+    // signoutButton.style.display = 'none';
+  }
+}
+
+/**
+       *  Sign in the user upon button click.
+       */
+export function handleAuthClick(event) {
+  window.gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  window.gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  console.log(message)
+  // var pre = document.getElementById('content');
+  // var textContent = document.createTextNode(message + '\n');
+  // pre.appendChild(textContent);
+}
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+function listUpcomingEvents() {
+  // https://stackoverflow.com/questions/29974011/try-to-display-calendar-list-from-google-api-using-java-script
+  var request = window.gapi.client.calendar.calendarList.list();
+  request.execute(function(resp){
+    var calendars = resp.items;
+    console.log(calendars);
+  });
+  
+  // window.gapi.client.calendar.events.list({
+  //   'calendarId': 'primary',
+  //   'timeMin': (new Date()).toISOString(),
+  //   'showDeleted': false,
+  //   'singleEvents': true,
+  //   'maxResults': 10,
+  //   'orderBy': 'startTime'
+  // }).then(function(response) {
+  //   var events = response.result.items;
+  //   appendPre('Upcoming events:');
+
+  //   if (events.length > 0) {
+  //     for (var i = 0; i < events.length; i++) {
+  //       var event = events[i];
+  //       var when = event.start.dateTime;
+  //       if (!when) {
+  //         when = event.start.date;
+  //       }
+  //       appendPre(event.summary + ' (' + when + ')')
+  //     }
+  //   } else {
+  //     appendPre('No upcoming events found.');
+  //   }
+  // });
+}
