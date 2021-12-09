ae7b3f498cccccdc6efa98bcd3b29b6a77dfc88c
Emily Fan <emilyfan8@gmail.com>
2021-12-09 00:16:47 -0500

    make todolist display in table format

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,32 +3,113 @@ import React, { useState, useEffect } from 'react';
 import firebase from '../firebase'
 import Todo from './Todo';
 
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+
+import { TableContainer, Table, TableRow, TableCell, TableBody, TableHead } from '@mui/material';
+import Paper from '@mui/material/Paper';
+
+
+const deleteTodo = (todo) => {
+    const todoRef = firebase.database().ref('Todo').child(todo.id);
+    todoRef.remove();
+}
+const completeTodo = (todo) => {
+    const todoRef = firebase.database().ref('Todo').child(todo.id);
+    todoRef.update({
+        complete: !todo.complete,
+    })
+}
+
 const TodoList = () => {
     const [todoList, setTodoList] = useState();
     useEffect(() => {
         const todoRef = firebase.database().ref('Todo');
         todoRef.on('value', (snapshot) => {
             const todos = snapshot.val();
+            console.log("Log todos", todos)
             const todoList = []
             for (let id in todos) {
+                console.log(id)
                 todoList.push({ id, ...todos[id] });
             }
+            console.log(todoList)
             setTodoList(todoList);
         })
     }, [])
+    
     return (
-        <>
-            <h2>TodoList</h2>
-            <motion.div
-                layout
-            >
-                {todoList ?
-                    todoList.map((todo, index) =>
-                        <Todo todo={todo} key={index} />
-                    )
-                    : ''}
-            </motion.div>
-        </>
+        <motion.div>
+        <h2>TodoList</h2>
+        <TableContainer>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell>Todo Item</TableCell>
+                <TableCell align="right">dueDate</TableCell>
+                <TableCell align="right">hardDeadline</TableCell>
+                <TableCell align="right">estTime</TableCell>
+                <TableCell align="right">priority</TableCell>
+                <TableCell align="right">overshoot</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {todoList ?
+                todoList.map((todo) => 
+                    <TableRow
+                    key={todo.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                    <TableCell>
+                        {todo.complete ?
+                            <CheckCircleIcon
+                                className='icon'
+                                onClick={ () => completeTodo(todo)}
+                                fontSize='large'
+                            /> :
+                            <CheckCircleOutlineIcon
+                                className='icon'
+                                onClick={ () => completeTodo(todo) }
+                                fontSize='large'
+                            />
+                         }
+                         {/* <motion.div> */}
+                            <HighlightOffIcon
+                                className='icon'
+                                onClick={ () => deleteTodo(todo) }
+                                fontSize='large'
+                            />
+                        {/* </motion.div> */}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                        {todo.title}
+                    </TableCell>
+                    <TableCell align="right">{todo.dueDate}</TableCell>
+                    <TableCell align="right">{todo.hardDeadline.toString()}</TableCell>
+                    <TableCell align="right">{todo.estTime}</TableCell>
+                    <TableCell align="right">{todo.priority}</TableCell>
+                    </TableRow>
+                )
+                : ''}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        </motion.div>
+        
+        // <>
+        //     <h2>TodoList</h2>
+        //     <motion.div
+        //         layout
+        //     >
+        //         {todoList ?
+        //             todoList.map((todo, index) =>
+        //                 <Todo todo={todo} key={index} />
+        //             )
+        //             : ''}
+        //     </motion.div>
+        // </>
     );
 }
 export default TodoList;
