0068806952e5612224601163836cd3f6451baf5c
Emily Fan <emilyfan8@gmail.com>
2021-12-08 23:51:16 -0500

    make Login Signout Buttons into classes

--- src/components/CalendarIntegration.js
+++ src/components/CalendarIntegration.js
@@ -4,26 +4,39 @@ import React from 'react'
 import CalendarSelection from './CalendarSelection'
 import { gapiSignin, gapiSignout, handleClientLoad, getCalendarList, loadGoogleScript } from '../utils/loadgs'
 
-const CalendarIntegration = () => {
-    return (
-        <> 
-        {/* This is necessary because JSX need parent */}
-            <Button
-                variant='contained'
-                id='authorize_button'
-                onClick={handleAuthClick}
-            >
-                Connect to GCAL
-            </Button>
 
-            <Button
-                variant='contained'
-                id='signout_button'
-                onClick={handleSignoutClick}
-            >
-                Sign Out
-            </Button>
-        </>
-    );
+function LoginButton(props){
+    return (
+        <Button
+            variant='contained'
+            id='authorize_button'
+            onClick={props.onClick}
+        >
+            Connect to GCAL
+        </Button>
+    )
+}
+
+function SignoutButton(props){
+    return (    
+        <Button
+            variant='contained'
+            id='signout_button'
+            onClick={props.onClick}
+        >
+            Sign Out
+        </Button>
+    )
+
+}
+
+function ShowCalendarButton(props){
+    return (
+        <Button
+            onClick={props.onClick}
+        >
+            Show / Hide Calendar Checkboxes
+        </Button>
+    )
 }
 export default CalendarIntegration;
