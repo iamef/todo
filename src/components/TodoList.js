68d463d21598bd1163c5ca29c917bcf36c3d5b20
Emily Fan <emilyfan8@gmail.com>
2022-01-02 21:27:28 -0800

    make TodoList into a class

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -21,101 +21,166 @@ function completeTodo(todo){
     })
 }
 
-function TodoList(props){
-    console.log(props)
-    
-    const [todoList, setTodoList] = useState();
-    useEffect(() => {
-        
-        firebase.database().ref('Todo').get().then((sometodos) =>
-            console.log("some todo", sometodos.val())
-        );
+class TodoList extends React.Component{
+    constructor(props){
+        super(props);
+        console.log("Todolist props", props);
+
+        this.state = {}
+    }
+
+    componentDidMount(){
+        // firebase.database().ref('Todo').get().then((sometodos) =>
+        //     console.log("some todo", sometodos.val())
+        // );
         
         const todoRef = firebase.database().ref('Todo');
         todoRef.on('value', (snapshot) => {
-            // console.log(snapshot)
+            // Snapshot returns a DataSnapshot object
+            // console.log("snapshot", snapshot) 
             
             const todos = snapshot.val();
-            // console.log("Log todos", todos)
+            console.log("Log todos", todos)
             // console.log("parsed todos", JSON.parse(todos))
-            
-            firebase.database().ref('Calendars').get().then((calendarsSnapshot) => {
-                var calendars = calendarsSnapshot.val();
+            const todoList = []
+            for (let id in todos) {
+                // console.log(id)
+                todoList.push({ id, ...todos[id] });
+            }
+            // console.log("todoList", todoList)
+            // calculateBuffer(todoList, calendars)
+            this.setState(todoList);
 
-                // console.log("Log todos", todos)
-                
 
-                const todoList = []
-                for (let id in todos) {
-                    // console.log(id)
-                    todoList.push({ id, ...todos[id] });
-                }
-                // console.log("todoList", todoList)
-                calculateBuffer(todoList, calendars)
-                setTodoList(todoList);
-            });
+            if(this.props.signedIn === true){
+                firebase.database().ref('Calendars').get().then((calendarsSnapshot) => {
+                    var calendars = calendarsSnapshot.val();
 
-        });
-    }, [])
+                    // console.log("Log todos", todos)
+                    
+
+                    const todoList = []
+                    for (let id in todos) {
+                        // console.log(id)
+                        todoList.push({ id, ...todos[id] });
+                    }
+                    // console.log("todoList", todoList)
+                    calculateBuffer(todoList, calendars)
+                    this.setState(todoList);
+                });
+            }
+
+        })
+    }
+
     
-    return (
-        <motion.div>
-        <h2>TodoList</h2>
-        <TableContainer>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell></TableCell>
-                <TableCell>Todo Item</TableCell>
-                <TableCell align="right">dueDate</TableCell>
-                <TableCell align="right">hardDeadline</TableCell>
-                <TableCell align="right">estTime</TableCell>
-                <TableCell align="right">priority</TableCell>
-                <TableCell align="right">overshoot</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {todoList ?
-                todoList.map((todo) => 
-                    <TableRow
-                    key={todo.id}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                    <TableCell>
-                        {todo.complete ?
-                            <CheckCircleIcon
-                                className='icon'
-                                onClick={ () => completeTodo(todo)}
-                                fontSize='large'
-                            /> :
-                            <CheckCircleOutlineIcon
-                                className='icon'
-                                onClick={ () => completeTodo(todo) }
-                                fontSize='large'
-                            />
-                         }
-                         {/* <motion.div> */}
-                            <HighlightOffIcon
-                                className='icon'
-                                onClick={ () => deleteTodo(todo) }
-                                fontSize='large'
-                            />
-                        {/* </motion.div> */}
-                    </TableCell>
-                    <TableCell component="th" scope="row">
-                        {todo.title}
-                    </TableCell>
-                    <TableCell align="right">{todo.dueDate}</TableCell>
-                    <TableCell align="right">{todo.deadlineType}</TableCell>
-                    <TableCell align="right">{todo.estTime}</TableCell>
-                    <TableCell align="right">{todo.priority}</TableCell>
-                    </TableRow>
-                )
-                : ''}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        </motion.div>
+    // useEffect(() => {
+        
+    //     // firebase.database().ref('Todo').get().then((sometodos) =>
+    //     //     console.log("some todo", sometodos.val())
+    //     // );
+        
+    //     const todoRef = firebase.database().ref('Todo');
+    //     todoRef.on('value', (snapshot) => {
+    //         // Snapshot returns a DataSnapshot object
+    //         // console.log("snapshot", snapshot) 
+            
+    //         const todos = snapshot.val();
+    //         console.log("Log todos", todos)
+    //         // console.log("parsed todos", JSON.parse(todos))
+    //         const todoList = []
+    //         for (let id in todos) {
+    //             // console.log(id)
+    //             todoList.push({ id, ...todos[id] });
+    //         }
+    //         // console.log("todoList", todoList)
+    //         // calculateBuffer(todoList, calendars)
+    //         setTodoList(todoList);
+
+
+    //         // if(props.signedIn === true){
+    //         //     firebase.database().ref('Calendars').get().then((calendarsSnapshot) => {
+    //         //         var calendars = calendarsSnapshot.val();
+
+    //         //         // console.log("Log todos", todos)
+                    
+
+    //         //         const todoList = []
+    //         //         for (let id in todos) {
+    //         //             // console.log(id)
+    //         //             todoList.push({ id, ...todos[id] });
+    //         //         }
+    //         //         // console.log("todoList", todoList)
+    //         //         calculateBuffer(todoList, calendars)
+    //         //         setTodoList(todoList);
+    //         //     });
+    //         // }
+
+    //     });
+    // }, [])
+    
+    render(){
+        return (
+            <motion.div>
+            <h2>TodoList</h2>
+            <TableContainer>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell></TableCell>
+                    <TableCell>Todo Item</TableCell>
+                    <TableCell align="right">dueDate</TableCell>
+                    <TableCell align="right">deadline</TableCell>
+                    <TableCell align="right">estTime</TableCell>
+                    <TableCell align="right">priority</TableCell>
+                    <TableCell align="right">overshoot</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {this.state ?
+                    this.state.map((todo) => 
+                        <TableRow
+                        key={todo.id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                        <TableCell>
+                            {todo.complete ?
+                                <CheckCircleIcon
+                                    className='icon'
+                                    onClick={ () => completeTodo(todo)}
+                                    fontSize='large'
+                                /> :
+                                <CheckCircleOutlineIcon
+                                    className='icon'
+                                    onClick={ () => completeTodo(todo) }
+                                    fontSize='large'
+                                />
+                            }
+                            {/* <motion.div> */}
+                                <HighlightOffIcon
+                                    className='icon'
+                                    onClick={ () => deleteTodo(todo) }
+                                    fontSize='large'
+                                />
+                            {/* </motion.div> */}
+                        </TableCell>
+                        <TableCell component="th" scope="row">
+                            {todo.title}
+                        </TableCell>
+                        <TableCell align="right">{todo.dueDate}</TableCell>
+                        <TableCell align="right">{todo.deadlineType}</TableCell>
+                        <TableCell align="right">{todo.estTime}</TableCell>
+                        <TableCell align="right">{todo.priority}</TableCell>
+                        <TableCell align="right">0</TableCell>
+                        </TableRow>
+                    )
+                    : null
+                }
+                </TableBody>
+            </Table>
+            </TableContainer>
+            </motion.div>
+        );
         
         // <>
         //     <h2>TodoList</h2>
@@ -129,6 +194,6 @@ function TodoList(props){
         //             : ''}
         //     </motion.div>
         // </>
-    );
+    }
 }
 export default TodoList;
